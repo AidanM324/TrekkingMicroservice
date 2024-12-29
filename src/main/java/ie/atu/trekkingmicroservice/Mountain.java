b4ae78ce24de827073537d3109ae540e72adace6
Aidan Malone <g00423306@atu.ie>
2024-12-29 21:58:06 +0000

    Created Jpa repo, created mountain class

--- src/main/java/ie/atu/trekkingmicroservice/Mountain.java
+++ src/main/java/ie/atu/trekkingmicroservice/Mountain.java
@@ -1,11 +1,50 @@
 package ie.atu.trekkingmicroservice;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Mountain {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long Id;
+
+    @NotBlank
+    private String mountain;
+
+    @NotBlank
+    private String company;
+
+    @Min(18)
+    private int priceRange;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String length;
+
+    @NotBlank
+    private String location;
+
+    @NotBlank
+    private String difficulty;
+
+    @NotBlank
+    private String mountainRange;
+
+
+
 }
